4f3707b5bd830e08cc8fb7dcf6438e173ff4d7c9
Miguel Prata Silva <48215025+MigSilverS@users.noreply.github.com>
2024-06-18 20:40:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function buscarCEP() {
+    let cep = document.getElementById('cep').value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            if (!data.erro) {
+                document.getElementById('rua').value = data.logradouro;
+                document.getElementById('bairro').value = data.bairro;
+                document.getElementById('cidade').value = data.localidade;
+                document.getElementById('estado').value = data.uf;
+            } else {
+                alert('CEP não encontrado.');
+            }
+        })
+        .catch(error => console.error('Erro ao buscar CEP:', error));
+}
